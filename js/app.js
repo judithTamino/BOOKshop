da32bff63b0b9ec2e55cb729f1be01a74463ae1f
יהודית טמינו <taminojudit@hotmail.co.il>
2023-10-01 14:40:52 +0300

    hero section design

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const menu = document.querySelector('.nav__menu');
-const toggle = document.querySelector('.nav__toggle');
+const toggleBtn = document.querySelector('.nav__toggle');
+const closeBtn = document.querySelector('.nav__close');
 
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('show__menu');
+toggleBtn.addEventListener('click', () => {
+  menu.classList.add('show__menu');
+})
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('show__menu');
 })
